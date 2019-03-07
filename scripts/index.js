5b16cebee5df3dc7b21b9514320081632bf8208a
Ashish Garg <mragarg@gmail.com>
2019-03-07 13:04:26 -0500

    First Commit

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+
+// Implement the following functions to fulfill the tests!
+function getLocationName() {}
+function getLocationCountry() {}
+function getLocationLatitude() {}
+function getLocationLongitude() {}
+function getDescription() {}
+function getWindSpeed() {}
+function getSunrise() {}
+
+
+// Please ignore the following
+try {
+    module.exports = {
+        getLocationName,
+        getLocationCountry,
+        getLocationLatitude,
+        getLocationLongitude,
+        getDescription,
+        getWindSpeed,
+        getSunrise
+    }
+} catch (e) {
+    
+}
