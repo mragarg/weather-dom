5e6941539ca389988df251dfe33e83a2f4f4e994
Ashish Garg <mragarg@gmail.com>
2019-03-11 15:04:15 -0400

    Used api to gather data

--- scripts/index.js
+++ scripts/index.js
@@ -63,30 +63,41 @@ try {
 const body = document.querySelector("body");
 const weatherInfo = document.querySelector("[data-weatherInfo]");
 const mapLocation = document.querySelector("[data-map]");
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=2d5f8fa271ed1d35a26ff618bbe03598";
+let theWeather;
+fetch(URL).then(function (response){ 
+    return response.json(); 
+}).then(function (data){
+    theWeather = data;
 
-// Go through data and create a `p` tag for them (city name, temperature, wind speed)
-const cityName = document.createElement("p");
-cityName.textContent = "City Name: " + getLocationName(atlWeather);
 
-const cityTemp = document.createElement("p");
-cityTemp.textContent = "Temperature: " + getLocationTemperature(atlWeather);
+    // Go through data and create a `p` tag for them (city name, temperature, wind speed)
+    const cityName = document.createElement("p");
+    cityName.textContent = "City Name: " + getLocationName(theWeather);
+    
+    const cityTemp = document.createElement("p");
+    cityTemp.textContent = "Temperature: " + getLocationTemperature(theWeather);
+    
+    const cityWindSpeed = document.createElement("p");
+    cityWindSpeed.textContent = "Wind Speed: " + getWindSpeed(theWeather);
+    
+    weatherInfo.appendChild(cityName);
+    weatherInfo.appendChild(cityTemp);
+    weatherInfo.appendChild(cityWindSpeed);
+    showWeatherIcon(getWeatherIcon(theWeather));
+    
+    mapLocation.src = "http://maps.google.com/maps?q=" + getLocationLatitude(theWeather) + ", " + getLocationLongitude(theWeather) +  "&z=15&output=embed";
+    
+    // Append them to the weatherDiv
+    // Append weatherDiv to body 
+    
+    // body.prepend(weatherDiv); 
+    // appendChild is after and prepend is before 
+    // NOTE: Remember to appendChild to body at the end to save computionally `energy`
+    body.appendChild(weatherInfo);
+    
+    console.log(getWeatherIcon(theWeather));
+    console.log(theWeather);
 
-const cityWindSpeed = document.createElement("p");
-cityWindSpeed.textContent = "Wind Speed: " + getWindSpeed(atlWeather);
-
-weatherInfo.appendChild(cityName);
-weatherInfo.appendChild(cityTemp);
-weatherInfo.appendChild(cityWindSpeed);
-showWeatherIcon(getWeatherIcon(atlWeather));
-
-mapLocation.src = "http://maps.google.com/maps?q=" + getLocationLatitude(atlWeather) + ", " + getLocationLongitude(atlWeather) +  "&z=15&output=embed";
-
-// Append them to the weatherDiv
-// Append weatherDiv to body 
-
-// body.prepend(weatherDiv); 
-// appendChild is after and prepend is before 
-// NOTE: Remember to appendChild to body at the end to save computionally `energy`
-body.appendChild(weatherInfo);
-
-console.log(getWeatherIcon(atlWeather));
+    
+});
